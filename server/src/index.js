191fae2c46472635634f88f13eb5194eabe26f9c
Eladio Rocha Vizcaino <eladio.rocha99@gmail.com>
2020-05-27 22:32:22 -0500

    Modified the main server files

--- server/src/index.js
+++ server/src/index.js
@@ -1,2 +1,5 @@
+const mongoose = require('mongoose')
 require('dotenv').config()
+
+mongoose.connect(process.env.MONGO_URI_DEV);
 require(require('path').join(__dirname, 'server')).listen(process.env.PORT || process.env.PORT_DEV, () => console.log('SERVER WORKS', process.env.PORT || process.env.PORT_DEV))
--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,15 @@
 const path = require('path'),
     bodyParser = require('body-parser'),
     morgan = require('morgan'),
-    cookieParser = require('cookie-parser'),
     express = require('express'),
     app = express();
 
 app.use(morgan('dev'))
-app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use('/api/v1/authentication', require(path.join(__dirname, 'services', 'authentication', 'routes')))
+app.use('/api/v1/books', require(path.join(__dirname, 'services', 'books', 'routes')))
 
 module.exports = app
 
