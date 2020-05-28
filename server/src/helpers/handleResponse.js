f9ec15df059c900529c6eace8d15e7a964303f58
Eladio Rocha Vizcaino <eladio.rocha99@gmail.com>
2020-05-28 18:44:21 -0500

    Added helper from send response

---
+++ server/src/helpers/handleResponse.js
@@ -0,0 +1,14 @@
+const path = require('path')
+    responses = require(path.join(__dirname, '..', 'responses.json'));
+
+function response(res, status, data = null, message = null) {
+    try {
+        return res.status(status).json({status: status, statusText: responses[status].text, data, message: (status === 500) ? responses[status].message : message})
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+module.exports = {
+    response
+}
