3a1f3996990c88976890da098922e0e8023bb05b
Eladio Rocha Vizcaino <eladio.rocha99@gmail.com>
2020-06-02 10:02:47 -0500

    Fixed text

--- server/src/models/StatusReports.js
+++ server/src/models/StatusReports.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const StatusReportsSchema = new mongoose.Schema({
     name: {type: String, required: true, maxlength: 50},
-    description: {type: String, required: true, maxlength: 255},
+    description: {type: String, maxlength: 255},
     created_at: {type: Date, required: true, default: Date.now},
     updated_at: {type: Date, required: true, default: Date.now}
 })
